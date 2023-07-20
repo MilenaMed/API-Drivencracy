62576b82edef041c6159301e171800a0fecfe41d
MilenaMed <milenamfm@gmail.com>
2023-07-20 16:45:34 -0300

    Post - poll
    titlenão pode ser uma string vazia, retornar status 422.
    Se expireAt for vazio deve ser considerado 30 dias de enquete por padrão.
    Deve retornar a enquete criada em caso de sucesso com status 201.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
+import joi from "joi";
+
 
 const app = express();
 
@@ -21,6 +24,40 @@ catch (err) {
 }
 
 const db = mongoClient.db()
+
+//Joi
+//const pollSchema = joi.object({
+   // title: joi.string().required(),
+//})
+
+//POST-poll
+app.post("/poll", async (request, response) => {
+    const poll = request.body
+
+    //const validation = pollSchema.validate(request.body)
+    //if (validation.error) {
+    //  return response.status(422).send("Title required")
+    // }
+
+    if (poll.title === "") {
+        return response.status(422).send("Title required")
+    }
+
+    try {
+        const pollSaved = {
+            title: poll.title,
+            expireAt: !poll.expireAt ? dayjs().add(30, "day").format(`YYYY/MM/DD HH:mm`) : poll.expireAt,
+        }
+
+        await db.collection("poll").insertOne(pollSaved)
+        console.log(pollSaved)
+        response.status(201).send("Poll registered")
+
+    } catch (err) {
+        response.status(500).send(err.message)
+    }
+})
+
 
 //Porta
 const porta = 5000
